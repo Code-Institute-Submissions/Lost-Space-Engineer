ce428921f9d04930eabd190da7b977a64d4ace15
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-20 11:48:06 +0000

    Add welcome message to run.py file

--- run.py
+++ run.py
@@ -1,1 +1,3 @@
-print("Welcome to the adventure game Lost Space Engineer")
+print("Welcome to the text adventure game Lost Space Engineer")
+print("The aim of the game is to explore the ship and fix the broken systems\n")
+print("Along the way you can collect tools to help fix the any broken systems")
