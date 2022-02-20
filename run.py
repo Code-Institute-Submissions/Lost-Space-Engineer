c5aaa93ab48f3f1547e516fc4aae8c7afcfa4270
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-20 14:17:11 +0000

    Add input for start game

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-print("Welcome to the text adventure game Lost Space Engineer")
-print("The aim of the game is to explore the ship and fix the broken systems\n")
-print("Along the way you can collect tools to help fix the any broken systems")
+def start_game():
+      """
+      looks for input from player to begin the adventure
+      """
+      print("Welcome to the text adventure game Lost Space Engineer\n")
+      print("The aim of the game is to explore the ship and "
+          "fix any broken sub-systems")
+      print("While exploring the ship you may find tools "
+          "to help fix any broken systems\n")
+      play = input("Are you ready? if so type 'play'\n")
+
+
+def main():
+      start_game()
+
+main()
