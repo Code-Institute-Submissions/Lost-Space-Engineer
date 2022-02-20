cbe244dd504c90096a7d93f3b22a4733bc3c4c94
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-20 14:39:19 +0000

    Add validation for input on ready to start

--- run.py
+++ run.py
@@ -4,10 +4,32 @@ def start_game():
       """
       print("Welcome to the text adventure game Lost Space Engineer\n")
       print("The aim of the game is to explore the ship and "
-          "fix any broken sub-systems")
+            "fix any broken sub-systems")
       print("While exploring the ship you may find tools "
-          "to help fix any broken systems\n")
-      play = input("Are you ready? if so type 'play'\n")
+            "to help fix any broken systems\n")
+      while True:
+            play = input("Are you ready? if so type 'play'\n")
+
+            if play_check(play):
+                  print("Good Luck and have fun!!")
+                  break
+
+
+
+def play_check(value):
+      """
+      Checks to see if the player has enter play and it's valid
+      """
+      try:
+            if value != "play":
+                  raise ValueError(
+                        "Please type 'play' in all lowercase"
+                  )
+      except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            return False
+
+      return True
 
 
 def main():
